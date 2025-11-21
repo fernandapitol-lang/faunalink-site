b0e603cc3ddffdce5599b5dad207b3ac8d7d1287
fernandapitol-lang <fernandapitol@gmail.com>
2025-11-21 16:45:54 -0300

    Delete libs directory

--- libs/loaders/jspdf-loader.js
+++
@@ -1,7 +0,0 @@
-
-(function(){
-  var cdn = 'https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js';
-  var s=document.createElement('script'); s.src=cdn; s.onload=function(){console.log('jsPDF loaded from CDN')}; s.onerror=function(){console.warn('jsPDF CDN failed')}; document.head.appendChild(s);
-  // autotable plugin CDN
-  var at = document.createElement('script'); at.src='https://cdn.jsdelivr.net/npm/jspdf-autotable@3.5.28/dist/jspdf.plugin.autotable.min.js'; document.head.appendChild(at);
-})();
